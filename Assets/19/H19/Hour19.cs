5d6a2ba04aebf72444b91cf213ea5db18ddab458
Thomas <1707235@mukilteo.wednet.edu>
2021-12-16 21:17:40 -0800

    no message

---
+++ Assets/19/H19/Hour19.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Hour19 : MonoBehaviour
+{
+   
+    PlayableDirector director;
+    void Start()
+    {
+        director = GetComponent<PlayableDirector>();
+    }
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (director.state == PlayState.Paused)
+                director.Play();
+            else
+                director.Stop();
+        }
+    }
+
+
+}
